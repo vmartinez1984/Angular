0adbbfc19f4dd836cc2880c1c9b45a7126d87b6b
victor bravo <ahal_tocob@hotmail.com>
2021-06-30 22:27:22 -0500

    Se agregaron validaciones

--- tarjetas/src/app/app.module.ts
+++ tarjetas/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +13,8 @@ import { TarjetaDeCreditoComponent } from './components/tarjeta-de-credito/tarje
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tarjetas/src/app/components/tarjeta-de-credito/tarjeta-de-credito.component.ts
+++ tarjetas/src/app/components/tarjeta-de-credito/tarjeta-de-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-de-credito',
@@ -7,13 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarjetaDeCreditoComponent implements OnInit {
   listaDeTarjetas: any[] = [
-    { titular:"Juan Hernández", numeroDeTarjeta:"1234 5678", fechaDeVencimiento:"11/21", cvv:"123"},
-    { titular:"María Hernández", numeroDeTarjeta:"1234 5678", fechaDeVencimiento:"11/22", cvv:"789"}
+    { titular: "Juan Hernández", numeroDeTarjeta: "1234 5678", fechaDeVencimiento: "11/21", cvv: "123" },
+    { titular: "María Hernández", numeroDeTarjeta: "1234 5678", fechaDeVencimiento: "11/22", cvv: "789" }
   ]
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: ['', Validators.required],
+      numeroDeTarjeta: ['',
+        [
+          Validators.required,
+          Validators.maxLength(16),
+          Validators.minLength(16)
+        ]
+      ],
+      fechaDeVencimiento: ['',
+        [
+          Validators.required,
+          Validators.maxLength(5),
+          Validators.minLength(5)
+        ]
+      ],
+      cvv: ['',
+      [
+        Validators.required,
+        Validators.maxLength(3),
+        Validators.minLength(3)
+      ]
+      ]
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  agregarTarjeta() {
+    //console.log(this.form);
+    const tarjeta: any = {
+      titular: this.form.get('titular')?.value,
+      numeroDeTarjeta: this.form.get('numeroDeTarjeta')?.value,
+      fechaDeVencimiento: this.form.get('fechaDeVencimiento')?.value,
+      cvv: this.form.get('cvv')?.value
+    }
+    //console.log(tarjeta);
+    this.listaDeTarjetas.push(tarjeta);
+    this.form.reset();
+  }
 }
